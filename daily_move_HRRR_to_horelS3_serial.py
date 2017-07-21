b21a1e9f8d884f8c56ce2dbad479fe27f12cab6f
Brian Blaylock <blaylockbk@gmail.com>
2017-07-21 15:52:57 -0600

    new script for refilling Pando with local files

--- daily_move_HRRR_to_horelS3_serial.py
+++ daily_move_HRRR_to_horelS3_serial.py
@@ -25,14 +25,17 @@ import stat
 
 # Date to transfer (yesterday's data)
 #DATE = datetime.today() - timedelta(days=1)
-DATE = datetime(2017, 1, 7)
+DATE = datetime(2017, 7, 19)
 
 # rclone config file
 config_file = '/uufs/chpc.utah.edu/sys/pkg/ldm/.rclone.conf' # meteo19 LDM user
 
-model_options = {1:'hrrr', 2:'hrrrX', 3:'hrrr_alaska'} # name in horel-group/archive
-model_S3_names = {1:'oper', 2:'exp', 3:'alaska'}       # name in horelS3:
+#model_options = {1:'hrrr', 2:'hrrrX', 3:'hrrr_alaska'} # name in horel-group/archive
+#model_S3_names = {1:'oper', 2:'exp', 3:'alaska'}       # name in horelS3:
 types = ['sfc', 'prs', 'buf']                          # model file types
+
+model_options = {1:'hrrr_alaska'} # name in horel-group/archive
+model_S3_names = {1:'alaska'}       # name in horelS3:
 
 def copy_to_horelS3(from_here, to_there):
     """
@@ -101,17 +104,14 @@ for M in model_options.keys():
     else:
         forecasts = np.arange(0, 19)
 
-    # Open file for printing output log. Organize into directories by year and month.
-    #log_path = 'logs/%s_%04d-%02d' % (model, DATE.year, DATE.month)
-    #if not os.path.exists(log_path):
-    #    os.makedirs(log_path)
-    #log = open('%s/%s_%04d-%02d-%02d.txt' % (log_path, model, DATE.year, DATE.month, DATE.day), 'w')
-    #log.write('Moving %s files\nDate: %s\n' % (model, DATE))
-    #log.write('Origin: ' + DIR)
-
     # Do lots of loops...file type (t), hour of day (h), forecast hour (f).
     # loop for each type: sfc, prs, buf
     for t in types:
+        # Files already on S3 (we don't want to repeat downloads)
+        s3_list = os.popen('rclone ls horelS3:HRRR/%s/%s/%04d%02d%02d/ | cut -c 11-' \
+                            % (model_S3_names[model_type], t, DATE.year, DATE.month, DATE.day)).read().split('\n')
+
+
         # Known conditions that don't exist
         if t == 'buf' and (model == 'hrrr_alaska' or model == 'hrrrX'):
             # no bufer files for alaska for experimental HRRR, so don't even check
@@ -120,12 +120,9 @@ for M in model_options.keys():
         # Build the new S3 directory path name (e.g. HRRR/oper/sfc/20171201)
         DIR_S3 = 'HRRR/%s/%s/%04d%02d%02d' \
                     % (model_S3_names[model_type], t, DATE.year, DATE.month, DATE.day)
-        #log.write('  \n\nCopy to: horelS3:'+DIR_S3+'\n')
-        #log.write("========== Checking for "+model + ' ' + t +" files ====================\n")
 
         # loop for each hour (0,24)
         for h in range(0, 24):
-            #log.write('Hour %02d:' % (h))
 
             # loop for each forecast hour, depenent on model type.
             for f in forecasts:
@@ -135,14 +132,14 @@ for M in model_options.keys():
                     # analysis and forecast are in the same file.
                     continue
 
-                print ""
-                print "===================================="
-                print "  Where am I?"
-                print "      Date  =", DATE
-                print "      model =", model
-                print "      type  =", t
-                print "      hour  =", h
-                print "      forec =", f
+                #print ""
+                #print "===================================="
+                #print "  Where am I?"
+                #print "      Date  =", DATE
+                #print "      model =", model
+                #print "      type  =", t
+                #print "      hour  =", h
+                #print "      forec =", f
 
                 # File path and name for hrrr and hrrrx (e.g hrrr.t00.wrfsfcf00.grib2)
                 if model == 'hrrr' or model == 'hrrrX':
@@ -158,7 +155,7 @@ for M in model_options.keys():
                         # Don't even try to get forecast prs fields
                         continue
 
-                # File patha and name for hrrr_alaska sfc (e.g hrrr_ak_prs_17010203_0012.grib2)
+                # File path and name for hrrr_alaska sfc (e.g hrrr_ak_prs_17010203_0012.grib2)
                 if model == 'hrrr_alaska' and t == 'sfc':
                     FILE = DIR + 'hrrr_ak_%s_%02d%02d%02d%02d_00%02d.grib2' \
                                 % (t, DATE.year-2000, DATE.month, DATE.day, h, f)
@@ -172,9 +169,7 @@ for M in model_options.keys():
                         if os.path.isfile(FILE):
                             # If the bufr file exists, then copy to S3
                             copy_to_horelS3(FILE, DIR_S3)
-                            #log.write('[%s]' % b)
-#                        else:
-#                            #log.write('[    ]')
+                            
                         # Change permissions of S3 directory to public
                         s3cmd = '/uufs/chpc.utah.edu/common/home/horel-group/archive_s3/s3cmd-1.6.1/s3cmd'
                         os.system(s3cmd + ' setacl s3://%s --acl-public --recursive' % DIR_S3)
@@ -185,20 +180,14 @@ for M in model_options.keys():
                     if model == 'hrrr_alaska':
                         # Rename the alaska files to match naming convention
                         rename_AK = 'hrrrAK.t%02dz.wrf%sf%02d.grib2' % (h, t, f)
-                        copy_to_horelS3_rename(FILE, DIR_S3, rename_AK)
+                        if rename_AK not in s3_list:
+                            #copy_to_horelS3_rename(FILE, DIR_S3, rename_AK)
+                            print 'copy %s as %s' % (FILE, rename_AK)
+                        else:
+                            print rename_AK, 'is in Pando already'
                     else:
                         copy_to_horelS3(FILE, DIR_S3)
-                    #log.write('[f%02d]' % (f))
- #               else:
-                    #log.write('[   ]')
-            #log.write('\n')
-    #log.close()
 
                 # Change permissions of S3 directory to public
                 s3cmd = '/uufs/chpc.utah.edu/common/home/horel-group/archive_s3/s3cmd-1.6.1/s3cmd'
                 os.system(s3cmd + ' setacl s3://%s --acl-public --recursive' % DIR_S3)
-
-
-# Meteo19 Crontab: 
-# Move HRRR files from horel-group/archive to S3 archive
-#21 12 * * * python /uufs/chpc.utah.edu/common/home/horel-group/archive_s3/daily_move_HRRR_to_horelS3_serial.py > /uufs/chpc.utah.edu/common/home/horel-group/archive_s3/daily_transfer.log
