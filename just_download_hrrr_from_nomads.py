b0cbaf10760542a92ccc112b1da491a8624890ac
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-02-12 12:06:28 -0700

    updates NEW PANDO

--- just_download_hrrr_from_nomads.py
+++ just_download_hrrr_from_nomads.py
@@ -2,17 +2,7 @@
 # March 9, 2017
 
 """
-Download archived HRRR files from MesoWest Pando S3 archive system.
-
-Please register before downloading from our HRRR archive:
-http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/hrrr_download_register.html
-
-For info on the University of Utah HRRR archive and to see what dates are 
-available, look here:
-http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/hrrr_FAQ.html
-
-Contact:
-brian.blaylock@utah.edu
+Download HRRR file from NOMADS
 """
 
 import urllib
@@ -36,10 +26,7 @@ DATE = date(2018, 1, 15)
 
 OUTDIR = '/uufs/chpc.utah.edu/common/home/horel-group/archive/%s/BB_test/models/%s' % (DATE.strftime('%Y%m%d'), model)
 
-
-
 # Loop through each hour and each forecast and download.
-
 for f in fxx:
     for h in hour:
         # 1) Build the URL string we want to download.
@@ -53,7 +40,7 @@ for f in fxx:
         print URL
         
         # 3) Download the file via https
-        # Check the file size, make it's big enough to exist.
+        # Check the file size, make sure it's big enough to exist.
         check_this = urllib.urlopen(URL)
         file_size = int(check_this.info()['content-length'])
         if file_size > 10000:
--- list_missing_files.py
+++ list_missing_files.py
@@ -9,16 +9,10 @@ import os
 from datetime import date, timedelta
 
 
-MODEL = 'hrrr'
+MODEL = 'hrrr'  # 'hrrr', 'hrrrak', 'hrrrX'
 fxx = 0
-level = 'sfc'
-
-if MODEL == 'hrrr':
-    MODEL_dir = 'oper'
-elif MODEL == 'hrrrX':
-    MODEL_dir = 'exp'
-elif MODEL == 'hrrrAK':
-    MODEL_dir = 'alaska'
+field = 'sfc'
+hours = range(0,24)
 
 sDATE = date(2015, 4, 18)
 eDATE = date.today()
@@ -26,12 +20,9 @@ days = (eDATE-sDATE).days
 DATES = [sDATE + timedelta(days=d) for d in range(days)]
 
 for D in DATES:
-    for h in range(24):
-        DIR = '/uufs/chpc.utah.edu/common/home/horel-group/archive/HRRR/%s/%s/%s/' % (MODEL_dir, level, D.strftime('%Y%m%d'))
-        FILE = '%s.t%02dz.wrf%sf%02d.grib2.idx' % (MODEL, h, level, fxx)
+    for h in hours:
+        DIR = '/uufs/chpc.utah.edu/common/home/horel-group7/Pando/%s/%s/%s/' % (MODEL, field, D.strftime('%Y%m%d'))
+        FILE = '%s.t%02dz.wrf%sf%02d.grib2' % (MODEL, h, level, fxx)
 
         if not os.path.exists(DIR+FILE):
-            print "FILE DOES NOT EXIST:", '%s/%s/%s/' % (MODEL_dir, level, D.strftime('%Y%m%d')), FILE 
-
-
-
+            print "FILE DOES NOT EXIST:", DIR+FILE
