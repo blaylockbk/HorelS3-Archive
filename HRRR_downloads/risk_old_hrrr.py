f7038ae5454090e07f0ee2068d8836fc8e6c054d
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-11-05 08:41:52 -0700

    updates

--- HRRR_downloads/risk_old_hrrr.py
+++ HRRR_downloads/risk_old_hrrr.py
@@ -8,11 +8,13 @@ few of the forecasts to reduce the amount of files on horel-group7. These files
 will still be available on Pando, as long as Pando stays a live and doesn't die
 again.
 
+WARNING: DO NOT SYNC HG7 TO PANDO AFTER FILES ARE REMOVED ON HG7
+
 I am KEEPING the following:
-    F00, F01, F02, F03, F06, F09, F12, F15, F18
+    F00, F01, F06, F12, F18
 
 I am RISKING the following:
-    F04, F05, F07, F08, F10, F11, F13, F14, F16, F17
+    F02, F03, F04, F05, F07, F08, F09, F10, F11, F13, F14, F15, F16, F17
 
 The risk files will only be removed if they are older than 600 days (23 months)
 """
@@ -29,13 +31,14 @@ if getpass.getuser() != 'mesohorse' or socket.gethostname() != 'meso1.chpc.utah.
 
 # List of dates from archive begining to 25 months ago. These are the dates we
 # want to risk on the Pando archive and not backup on horel-group7.
+#sDATE = datetime(2016, 7, 15) # Begining of archive...
 sDATE = datetime.now()-timedelta(days=602)
 eDATE = datetime.now()-timedelta(days=600)
 DATES = [sDATE + timedelta(days=d) for d in range((eDATE-sDATE).days)]
 
 # In each DATE directory, remove the files we want to risk
 
-RISK = ['f04', 'f05', 'f07', 'f08', 'f10', 'f11', 'f13', 'f14', 'f16', 'f17']
+RISK = ['f02', 'f03', 'f04', 'f05', 'f07', 'f08', 'f09', 'f10', 'f11', 'f13', 'f14', 'f15', 'f16', 'f17']
 
 for D in DATES:
     DIR = '/uufs/chpc.utah.edu/common/home/horel-group7/Pando/hrrr/sfc/%s/' % D.strftime('%Y%m%d')
